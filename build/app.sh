414b0707114f8a31d56b834b627384ddee345be3
Alexis <al.dacosta@f5.com>
2022-04-06 23:34:25 +0200

    u+x

--- build/app.sh
+++ build/app.sh
@@ -1,5 +1,4 @@
-export FAAS_APP_NAME=logstream-xc
-curl -X PUT --data-binary @${FAAS_APP_NAME}.json http://localhost:8080/config/
+curl -X PUT --data-binary @/docker-entrypoint.d/${FAAS_APP_NAME}.json http://localhost:8080/config/
 # curl -X PUT --data-binary @${FAAS_APP_NAME}.json --unix-socket /unit/control.unit.sock http://localhost/config/
 
 
--- build/tls.sh
+++ build/tls.sh
@@ -1,5 +1,4 @@
-export FAAS_APP_NAME=logstream-xc
-curl -X PUT --data-binary @${FAAS_APP_NAME}.pem http://localhost:8080/certificates/${FAAS_APP_NAME}
+curl -X PUT --data-binary @/docker-entrypoint.d/${FAAS_APP_NAME}.pem http://localhost:8080/certificates/${FAAS_APP_NAME}
 # curl -X PUT --data-binary @${FAAS_APP_NAME}.pem --unix-socket /unit/control.unit.sock http://localhost/certificates/${FAAS_APP_NAME}
 
 
