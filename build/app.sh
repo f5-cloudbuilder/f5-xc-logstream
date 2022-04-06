16f993c715db581f15af2e466cca8f706b11da24
Alexis <al.dacosta@f5.com>
2022-04-06 23:39:40 +0200

    8000

--- build/app.sh
+++ build/app.sh
@@ -1,4 +1,4 @@
-curl -X PUT --data-binary @/docker-entrypoint.d/${FAAS_APP_NAME}.json http://localhost:8080/config/
+curl -X PUT --data-binary @/docker-entrypoint.d/${FAAS_APP_NAME}.json http://localhost:8000/config/
 # curl -X PUT --data-binary @${FAAS_APP_NAME}.json --unix-socket /unit/control.unit.sock http://localhost/config/
 
 
