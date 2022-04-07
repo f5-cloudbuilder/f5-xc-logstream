18011bacd191b6b9b0e25088de3d40e260392555
Alexis <al.dacosta@f5.com>
2022-04-07 22:42:24 +0200

    socket

--- build/logstream-xc.sh
+++ build/logstream-xc.sh
@@ -1,5 +1,5 @@
-curl -X PUT --data-binary @/docker-entrypoint.d/${FAAS_APP_NAME}.pem http://localhost:8000/certificates/${FAAS_APP_NAME}
-curl -X PUT --data-binary @/docker-entrypoint.d/${FAAS_APP_NAME}.json http://localhost:8000/config/
+curl -X PUT --data-binary @/docker-entrypoint.d/${FAAS_APP_NAME}.pem --unix-socket /unit/control.unit.sock http://localhost/certificates/${FAAS_APP_NAME}
+curl -X PUT --data-binary @/docker-entrypoint.d/${FAAS_APP_NAME}.json --unix-socket /unit/control.unit.sock http://localhost/config/
 
 
 
