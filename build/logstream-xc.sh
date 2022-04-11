732d3cc18b9362aefa953f58514583ebd984ec16
Alexis <al.dacosta@f5.com>
2022-04-11 12:43:28 +0200

    CMD docker-entrypoint.d use sock

--- build/logstream-xc.sh
+++ build/logstream-xc.sh
@@ -1,6 +1,6 @@
 export FAAS_APP_NAME=logstream-xc
-curl -X PUT --data-binary @/docker-entrypoint.d/${FAAS_APP_NAME}.pem http://localhost:8000/certificates/${FAAS_APP_NAME}
-curl -X PUT --data-binary @/docker-entrypoint.d/${FAAS_APP_NAME}.json http://localhost:8000/config/
+curl -X PUT --unix-socket /unit/control.unit.sock --data-binary @/docker-entrypoint.d/${FAAS_APP_NAME}.pem http://localhost/certificates/${FAAS_APP_NAME}
+curl -X PUT --unix-socket /unit/control.unit.sock --data-binary @/docker-entrypoint.d/${FAAS_APP_NAME}.json http://localhost/config/
 
 
 
