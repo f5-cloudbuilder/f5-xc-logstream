73729ce67bc3fb5fae3f99ce1eb3fd3a0f181bf9
Alexis <al.dacosta@f5.com>
2022-04-11 15:02:54 +0200

    1 CMD

--- build/logstream-xc.sh
+++ build/logstream-xc.sh
@@ -1,5 +1,5 @@
 echo "test" > /unit/test1
-unitd --no-daemon --control 0.0.0.0:8000 --pid /unit/unit.pid --state /unit/ --tmp /unit/ --log /unit/unit.log
+unitd --control 0.0.0.0:8000 --pid /unit/unit.pid --state /unit/ --tmp /unit/ --log /unit/unit.log
 echo "test" > /unit/test2
 export FAAS_APP_NAME=logstream-xc
 echo "test" > /unit/test3
