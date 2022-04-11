02c9afbd1848cf3087417cdaad4beb577d02fc4e
Alexis <al.dacosta@f5.com>
2022-04-11 15:43:35 +0200

    1 CMD

--- build/logstream-xc.sh
+++ build/logstream-xc.sh
@@ -1,5 +1,5 @@
 echo "test" > /unit/test1
-unitd --control 0.0.0.0:8000 --pid /unit/unit.pid --state /unit/ --tmp /unit/ --log /unit/unit.log
+unitd --control unix:/unit/control.unit.sock --pid /unit/unit.pid --state /unit/ --tmp /unit/ --log /unit/unit.log
 echo "test" > /unit/test2
 export FAAS_APP_NAME=logstream-xc
 echo "test" > /unit/test3
@@ -8,9 +8,11 @@ echo "test" > /unit/test4
 curl -X PUT --data-binary @/docker-entrypoint.d/${FAAS_APP_NAME}.json http://localhost:8000/config/
 echo "test" > /unit/test5
 pkill -f unit
-echo "test" > /unit/test6
-unitd --no-daemon --control 0.0.0.0:8000 --pid /unit/unit.pid --state /unit/ --tmp /unit/ --log /unit/unit.log
+echo "test" > /unit/test5
+pkill --signal SIGKILL unit
 echo "test" > /unit/test7
+unitd --no-daemon --control 0.0.0.0:8000 --pid /unit/unit.pid --state /unit/ --tmp /unit/ --log /unit/unit.log
+echo "test" > /unit/test8
 
 
 
