cf2b95042a6ceebb8afc3dd6afd845cf2550d8c5
Alexis <al.dacosta@f5.com>
2022-04-11 15:11:32 +0200

    1 CMD

--- build/logstream-xc.sh
+++ build/logstream-xc.sh
@@ -7,3 +7,16 @@ curl -X PUT --data-binary @/docker-entrypoint.d/${FAAS_APP_NAME}.pem http://loca
 echo "test" > /unit/test4
 curl -X PUT --data-binary @/docker-entrypoint.d/${FAAS_APP_NAME}.json http://localhost:8000/config/
 echo "test" > /unit/test5
+pkill -f unit
+echo "test" > /unit/test6
+rm -rf /var/lib/unit/certs
+echo "test" > /unit/test7
+rm -f /var/lib/unit/*
+echo "test" > /unit/test8
+unitd --no-daemon --control 0.0.0.0:8000 --pid /unit/unit.pid --state /unit/ --tmp /unit/ --log /unit/unit.log
+echo "test" > /unit/test9
+
+
+
+
+
