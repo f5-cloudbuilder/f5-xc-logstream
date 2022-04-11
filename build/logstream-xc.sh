06b22d4db4a7e4d632924d94781c95bb3f73b223
Alexis <al.dacosta@f5.com>
2022-04-11 16:01:25 +0200

    1 CMD

--- build/logstream-xc.sh
+++ build/logstream-xc.sh
@@ -1,18 +1,16 @@
-echo "test" > /unit/test1
+echo "Start Unit" > /unit/docker-entrypoint.log
 unitd --control 0.0.0.0:8000 --pid /unit/unit.pid --state /unit/ --tmp /unit/ --log /unit/unit.log
-echo "test" > /unit/test2
+echo "Configure Unit certificate" >> /unit/docker-entrypoint.log
 export FAAS_APP_NAME=logstream-xc
-echo "test" > /unit/test3
 curl -X PUT --data-binary @/docker-entrypoint.d/${FAAS_APP_NAME}.pem http://localhost:8000/certificates/${FAAS_APP_NAME}
-echo "test" > /unit/test4
+echo "Configure Unit App" >> /unit/docker-entrypoint.log
 curl -X PUT --data-binary @/docker-entrypoint.d/${FAAS_APP_NAME}.json http://localhost:8000/config/
-echo "test" > /unit/test5
-pkill -f unit
-echo "test" > /unit/test5
-pkill --signal SIGKILL unit
-echo "test" > /unit/test7
-unitd --no-daemon --control 0.0.0.0:8000 --pid /unit/unit.pid --state /unit/ --tmp /unit/ --log /unit/unit.log
-echo "test" > /unit/test8
+#echo "Stop Unit" >> /unit/docker-entrypoint.log
+#pkill -f unit
+#pkill --signal SIGKILL unit
+#echo "Start Unit as non-daemon" >> /unit/docker-entrypoint.log
+#unitd --no-daemon --control 0.0.0.0:8000 --pid /unit/unit.pid --state /unit/ --tmp /unit/ --log /unit/unit.log
+echo "End" >> /unit/docker-entrypoint.log
 
 
 
