c316da7890567959132693cde59deb1672b00497
Alexis <al.dacosta@f5.com>
2022-04-11 14:51:44 +0200

    1 CMD

--- build/logstream-xc.sh
+++ build/logstream-xc.sh
@@ -1,5 +1,3 @@
-pkill -f unit
-unitd --no-daemon --control 0.0.0.0:8000 --pid /unit/unit.pid --state /unit/ --tmp /unit/ --log /unit/unit.log
 export FAAS_APP_NAME=logstream-xc
 curl -X PUT --data-binary @/docker-entrypoint.d/${FAAS_APP_NAME}.pem http://localhost:8000/certificates/${FAAS_APP_NAME}
 curl -X PUT --data-binary @/docker-entrypoint.d/${FAAS_APP_NAME}.json http://localhost:8000/config/
